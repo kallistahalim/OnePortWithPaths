1480333bc6c1a8a5872d485af0b88dea12549d3f
kallistahalim <kallistahalim@gmail.com>
2020-05-21 16:38:02 -0700

    work

--- server.js
+++ server.js
@@ -23,3 +23,22 @@ function handleRequest(req, res) {
     }
 }
 
+function displayRoot(url, req, res) {
+    var myHTML = "<html>" + "<body><H1>HomePage</H1>" + "<a href = '/portfolio'>Portfolio</a>" + "</body></html>";
+    res.writeHead(200, { "content-type" : "text/html"});
+    res.end(myHTML);
+};
+
+function displayPortfolio(url, req, res) {
+    var myHTML = "<html>" + "<body>" + "<h1>Portfolio</h1>" + "<a href = '/'>Home Page</a>" + "</body>" + "</html>";
+    res.writeHead(200, { "content-type" : "text/html"});
+    res.end(myHTML);
+}
+
+function display404(url,req, res) {
+    var myHTML =  "<html>" + "<body>" + "<h1>404 not found</h1>" + "<p>The page you are looking for " + "http://localhost:" +PORT + url + " can not be found</p>" + "</body>" + "</html>";
+    res.writeHead(404, { "content-type" : "text/html"});
+    res.end(myHTML);
+}
+
+
